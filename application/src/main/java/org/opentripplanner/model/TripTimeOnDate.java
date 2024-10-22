19b4bfe4f732a10b908c4c6dbf6fe7f0ed1b35f9
Michael Tsang <michael.tsang@jnction.co.uk>
2024-10-22 14:44:29 +0100

    PR rejected - revert changes

--- application/src/main/java/org/opentripplanner/model/TripTimeOnDate.java
+++ application/src/main/java/org/opentripplanner/model/TripTimeOnDate.java
@@ -63,9 +63,6 @@ public class TripTimeOnDate {
   public static List<TripTimeOnDate> fromTripTimes(Timetable table, Trip trip) {
     TripTimes times = table.getTripTimes(trip);
     List<TripTimeOnDate> out = new ArrayList<>();
-    if (times == null) {
-      return out;
-    }
     for (int i = 0; i < times.getNumStops(); ++i) {
       out.add(new TripTimeOnDate(times, i, table.getPattern()));
     }
@@ -86,9 +83,6 @@ public class TripTimeOnDate {
   ) {
     TripTimes times = table.getTripTimes(trip);
     List<TripTimeOnDate> out = new ArrayList<>();
-    if (times == null) {
-      return out;
-    }
     for (int i = 0; i < times.getNumStops(); ++i) {
       out.add(new TripTimeOnDate(times, i, table.getPattern(), serviceDate, midnight));
     }
